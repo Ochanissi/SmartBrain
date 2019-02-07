2dbafacfd6238db11a1566a9f37180ed4fc00364
Ochanissi <mirelalex2094@gmail.com>
2019-02-07 10:23:52 +0200

    cleaning the code a little

--- src/App.js
+++ src/App.js
@@ -81,13 +81,14 @@ class App extends Component {
 
 
   render() {
+    const { isSignedIn, imageUrl, route, box } = this.state;
     return (
       <div className="App">
              <Particles  className='particles'
               params={particlesOptions}
             />
-        <Navigation isSignedIn={this.state.isSignedIn} onRouteChange={this.onRouteChange} />
-        { this.state.route === 'home'
+        <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange} />
+        { route === 'home'
           ? <div> 
           <Logo />
           <Rank />
@@ -95,10 +96,10 @@ class App extends Component {
             onInputChange={this.onInputChange}
             onButtonSubmit={this.onButtonSubmit}
           />
-          <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
+          <FaceRecognition box={box} imageUrl={imageUrl} />
     </div> 
           : (
-            this.state.route === 'signin'
+            route === 'signin'
             ? <Signin onRouteChange={this.onRouteChange}/>
             : <Register onRouteChange={this.onRouteChange}/>
           ) 
