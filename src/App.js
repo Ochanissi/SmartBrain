e806edd93293726cbc397869614a89b17793c49a
Ochanissi <mirelalex2094@gmail.com>
2019-07-07 22:56:43 +0300

    client session management

--- src/App.js
+++ src/App.js
@@ -45,6 +45,26 @@ class App extends Component {
     this.state = initialState;
   }
 
+  componentDidMount() {
+    const token = window.sessionStorage.getItem('token');
+    if (token) {
+      fetch('http://localhost:3000/signin', {
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': token
+        }
+      })
+        .then(resp => resp.json())
+        .then(data => {
+          if (data && data.id) {
+            console.log('Success! We need to get user progile.')
+          }
+        })
+          .catch(console.log)
+    }
+  }
+
   loadUser = (data) => {
     this.setState({user: {
       id: data.id,
