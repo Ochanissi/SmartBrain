78949accb91040b20ca705a67362ad85a5a720eb
Mirel Home <mirelalex2094@gmail.com>
2020-03-30 21:57:19 +0300

    added serve, cleaning up the code, logo, favicon, name, deploy to heroku

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,5 +1,6 @@
-import React, { Component } from "react";
-import "./Profile.css";
+import React, { Component } from 'react';
+import './Profile.css';
+import accounticon from './default.jpg';
 
 class Profile extends Component {
   constructor(props) {
@@ -13,12 +14,12 @@ class Profile extends Component {
 
   onProfileUpdate = data => {
     fetch(
-      `https://smartbrain-apps.herokuapp.com/profile/${this.props.user.id}`,
+      `https://smartbrain-apps-be.herokuapp.com/profile/${this.props.user.id}`,
       {
-        method: "post",
+        method: 'post',
         headers: {
-          "Content-Type": "application/json",
-          Authorization: window.sessionStorage.getItem("token")
+          'Content-Type': 'application/json',
+          Authorization: window.sessionStorage.getItem('token')
         },
         body: JSON.stringify({
           formInput: data
@@ -36,13 +37,13 @@ class Profile extends Component {
 
   onFormChange = event => {
     switch (event.target.name) {
-      case "user-name":
+      case 'user-name':
         this.setState({ name: event.target.value });
         break;
-      case "user-age":
+      case 'user-age':
         this.setState({ age: event.target.value });
         break;
-      case "user-pet":
+      case 'user-pet':
         this.setState({ pet: event.target.value });
         break;
       default:
@@ -54,13 +55,14 @@ class Profile extends Component {
     const { toggleModal, user } = this.props;
     const { name, age, pet } = this.state;
     return (
-      <div className="profile-modal">
-        <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center bg-white">
-          <main className="pa4 black-80 w-80">
+      <div className='profile-modal'>
+        <article className='br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center bg-white'>
+          <main className='pa4 black-80 w-80'>
             <img
-              src="http://tachyons.io/img/logo.jpg"
-              className="h3 w3 dib"
-              alt="avatar"
+              src={accounticon}
+              className='h3 w3 dib'
+              style={{ border: '2px solid #FFB6C1' }}
+              alt='avatar'
             />
             <h1>{name}</h1>
             <h4>{`Images submitted: ${user.entries}`}</h4>
@@ -68,55 +70,58 @@ class Profile extends Component {
               user.joined
             ).toLocaleDateString()}`}</p>
             <hr />
-            <label className="mt2 fw6" htmlFor="user-name">
+            <label className='mt2 fw6' htmlFor='user-name'>
               Name:
             </label>
             <input
               onChange={this.onFormChange}
-              type="text"
-              name="user-name"
-              className="pa2 ba w-100"
-              placeholder={name}
+              type='text'
+              name='user-name'
+              className='pa2 ba w-100'
+              placeholder='Tell us your name!'
+              value={name}
             ></input>
-            <label className="mt2 fw6" htmlFor="user-age">
+            <label className='mt2 fw6' htmlFor='user-age'>
               Age:
             </label>
             <input
               onChange={this.onFormChange}
-              type="text"
-              name="user-age"
-              className="pa2 ba w-100"
-              placeholder={age}
+              type='text'
+              name='user-age'
+              className='pa2 ba w-100'
+              placeholder='How old are you?'
+              value={age ? age : ''}
             ></input>
-            <label className="mt2 fw6" htmlFor="user-pet">
+            <label className='mt2 fw6' htmlFor='user-pet'>
               Favourite Pet:
             </label>
             <input
               onChange={this.onFormChange}
-              type="text"
-              name="user-pet"
-              className="pa2 ba w-100"
-              placeholder={pet}
+              type='text'
+              name='user-pet'
+              className='pa2 ba w-100'
+              placeholder='What is your favorite pet?'
+              value={pet ? pet : ''}
             ></input>
             <div
-              className="mt4"
-              style={{ display: "flex", justifyContent: "space-evenly" }}
+              className='mt4'
+              style={{ display: 'flex', justifyContent: 'space-evenly' }}
             >
               <button
-                className="b pa2 grow pointer hover-white w-40 bg-light-blue b--black-20"
+                className='b pa2 grow pointer hover-white w-40 bg-light-blue b--black-20'
                 onClick={() => this.onProfileUpdate({ name, age, pet })}
               >
                 Save
               </button>
               <button
-                className="b pa2 grow pointer hover-white w-40 bg-light-red b--black-20"
+                className='b pa2 grow pointer hover-white w-40 bg-light-red b--black-20'
                 onClick={toggleModal}
               >
                 Cancel
               </button>
             </div>
           </main>
-          <div className="modal-close" onClick={toggleModal}>
+          <div className='modal-close' onClick={toggleModal}>
             &times;
           </div>
         </article>
--- src/components/Profile/profileicon.js
+++ src/components/Profile/profileicon.js
@@ -1,5 +1,11 @@
 import React from 'react';
-import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
+import {
+  Dropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem
+} from 'reactstrap';
+import accounticon from './default.jpg';
 
 class ProfileIcon extends React.Component {
   constructor(props) {
@@ -13,27 +19,43 @@ class ProfileIcon extends React.Component {
     this.setState({
       dropdownOpen: !this.state.dropdownOpen
     });
-  }
+  };
 
   render() {
     return (
-      <div className="pa4 tc">
-            <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
-              <DropdownToggle
-                tag="span"
-                onClick={this.toggle}
-                data-toggle="dropdown"
-                aria-expanded={this.state.dropdownOpen}
-              >
-                <img
-                  src="http://tachyons.io/img/logo.jpg"
-                  className="br-100 h3 w3 dib" alt="avatar" />
-              </DropdownToggle>
-              <DropdownMenu className='b--transparent shadow-5' style={{marginTop: '20px', backgroundColor: 'rgba(255, 255, 255, 0.5)'}} right>
-                <DropdownItem onClick={() => this.props.toggleModal()}>View Profile</DropdownItem>
-                <DropdownItem onClick={() => this.props.onRouteChange('signout')}>Sign Out</DropdownItem>
-              </DropdownMenu>
-            </Dropdown>
+      <div className='pa4 tc'>
+        <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
+          <DropdownToggle
+            tag='span'
+            onClick={this.toggle}
+            data-toggle='dropdown'
+            aria-expanded={this.state.dropdownOpen}
+          >
+            <img
+              src={accounticon}
+              className='br4 h3 w3 dib'
+              style={{ border: '2px solid #FFB6C1' }}
+              alt='avatar'
+            />
+          </DropdownToggle>
+          <DropdownMenu
+            className='b--transparent shadow-5'
+            style={{
+              marginTop: '15px',
+              backgroundColor: 'rgba(255, 255, 255, 0.5)',
+              right: '0',
+              left: 'auto'
+            }}
+            right
+          >
+            <DropdownItem onClick={() => this.props.toggleModal()}>
+              View Profile
+            </DropdownItem>
+            <DropdownItem onClick={() => this.props.onRouteChange('signout')}>
+              Sign Out
+            </DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
       </div>
     );
   }
